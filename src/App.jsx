2c2a36ed84e6c59043128028c004a3d192f23e08
samueljsong <semuall0108@gmail.com>
2023-07-19 15:26:55 -0700

    created dark mode and light mode

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import './App.css'
+import LoginPage from './pages/LoginPage'
+
+function App() {
+
+  /* STATES */
+  const [mode, setMode] = useState("dark")
+  const [isDark, setDarkMode] = useState(true);
+
+
+  /* onClick Handlers */
+  const themeHandler = () => {
+    if(isDark){
+      setMode("light");
+      setDarkMode(false);
+    }else{
+      setMode("dark");
+      setDarkMode("true");
+    }
+  };
+
+  return (
+    <div className={`container ${mode}`}>
+      <LoginPage></LoginPage>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,14 @@
+import "./LoginPage.css"
+
+const LoginPage = () => {
+
+    return (
+        
+        <div className="login-container card">
+            <h1>Welcome</h1>
+        </div>
+        
+    )
+};
+
+export default LoginPage;
