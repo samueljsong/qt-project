8cf2193b2e3eb106a516693f0dd1095709c04def
semual <semuall0108@gmail.com>
2023-07-19 22:31:06 -0700

    minor tweeks to login pages

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,22 +2,31 @@ import './Login.css';
 
 const Login = (props) => {
 
-    return (
-        <div className='login-container card'>
-            <h1 className='login-title'>Quiet Time</h1>
-            <p>Log into Quiet Time</p>
+    const onClickAnimationHandler = (e) => {
+        console.log()
+    }
 
-            <form className='login-form' action="">
-                <input className={`${props.theme}-input`} type="email" required placeholder='Email'/>
-                <input className={`${props.theme}-input`} type="password" placeholder='Password' required />
-                <button className='login-button'>Log in</button>
-            </form>
-            <div className='login-segment'>
-                <span className='line'></span>
-                <p className='login-or-text'>or</p>
-                <span className='line'></span>
+
+    return (
+        <div className='login-container'>
+            <div className='square'></div>
+            <div className='circle'></div>
+            <div className="triangle"></div>
+            <div className={`login-card-container ${props.theme}-card`}>
+                <h1 className='login-title'>Quiet Time</h1>
+                <p>Log into Quiet Time</p>
+                <form className='login-form' action="">
+                    <input className={`${props.theme}-input`} type="email" required placeholder='Email'/>
+                    <input className={`${props.theme}-input`} type="password" placeholder='Password' required />
+                    <button onClick={onClickAnimationHandler} className='login-button'>Log in</button>
+                </form>
+                <div className='login-segment'>
+                    <span className='line'></span>
+                    <p className='login-or-text'>or</p>
+                    <span className='line'></span>
+                </div>
+                <button className='create-new-account-button'>Create new account</button>
             </div>
-            <button className='create-new-account-button'>Create new account</button>
         </div>
     )
 }
