752d5ecaf1a0d941620aca9b426e8de7b881b0a8
semual <semuall0108@gmail.com>
2023-07-20 22:54:24 -0700

    worked on desktop landing page and scrollbar

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -39,19 +39,27 @@ const LandingPage = (props) => {
 
     return(
         <div className='landing-container'>
-            <div className='todays-message-container'>
-                <p>Today's message is</p>
-                <p className='bible-verse'>Matthew 1:1-24</p>
-                <button className='button post-button'>Post your thoughts</button>
+            <div className={`profile-section ${props.theme}-card`}>
+
             </div>
-            <div className='post-container'>
-                {
-                    temporaryData.map(post => {
-                        return <PostCard theme={props.theme} user={post.user} 
-                                timePosted={post.timePosted} message={post.message}
-                                post={post.post} pic={post.pic}/>
-                    })
-                }
+            <div className='middle-section'>
+                <div className='todays-message-container'>
+                    <p>Today's message is</p>
+                    <p className='bible-verse'>Matthew 1:1-24</p>
+                    <button className='button post-button'>Post your thoughts</button>
+                </div>
+                <div className='post-container'>
+                    {
+                        temporaryData.map(post => {
+                            return <PostCard theme={props.theme} user={post.user} 
+                                    timePosted={post.timePosted} message={post.message}
+                                    post={post.post} pic={post.pic}/>
+                        })
+                    }
+                </div>
+            </div>
+            <div className={`friends-section ${props.theme}-card`}>
+
             </div>
         </div>
     )
