925ecd3d07a02507326be29eba103d9f9c175539
samueljsong <semuall0108@gmail.com>
2023-07-24 11:49:49 -0700

    checking express with front end

---
+++ server/netlify/functions/api.js
@@ -0,0 +1,11 @@
+import express, { Router } from 'express';
+import serverless from 'serverless-http';
+
+const api = express();
+
+const router = Router();
+router.get('/', (req, res) => res.send('Hello world'));
+
+api.use('/api/', router);
+
+export const handler = serverless(api);
