90c31bbf499e6b23935d053a142f328006989532
samueljsong <semuall0108@gmail.com>
2023-07-24 11:54:29 -0700

    checking to see backend

--- server/netlify/functions/api.js
+++ server/netlify/functions/api.js
@@ -4,7 +4,7 @@ import serverless from 'serverless-http';
 const api = express();
 
 const router = Router();
-router.get('/hello', (req, res) => res.send('Hello world'));
+router.get('/', (req, res) => res.send('Hello world'));
 
 api.use('/api/', router);
 
