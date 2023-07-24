e576425d9a1fa4c34ef2ae5307cd2f535f226545
samueljsong <semuall0108@gmail.com>
2023-07-24 11:59:04 -0700

    push

--- server/netlify/functions/api.js
+++ server/netlify/functions/api.js
@@ -4,7 +4,10 @@ import serverless from 'serverless-http';
 const api = express();
 
 const router = Router();
-router.get('/', (req, res) => res.send('Hello world'));
+router.get('/', (req, res) => {
+    res.send('Hello world')
+    console.log("LINKED")
+});
 
 api.use('/', router);
 
