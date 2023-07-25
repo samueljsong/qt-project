d65d47b7e42f52542e47c3a0251af1c0d0f7f3c4
samueljsong <semuall0108@gmail.com>
2023-07-24 17:12:55 -0700

    saved

--- server/server.js
+++ server/server.js
@@ -1,1 +1,11 @@
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect
