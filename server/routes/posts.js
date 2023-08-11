f8974fbdeaf9fe9998f0a01a57b77fab32968146
semual <semuall0108@gmail.com>
2023-08-10 23:25:44 -0700

    comment

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,6 +1,9 @@
 const router = require('express').Router;
 let Post = require('../models/post.model')
 
+/**
+ * This router request will do an API fetch to mongodb for all the posts for that user
+ */
 router.route('/').get((req, res) => {
     
 })
